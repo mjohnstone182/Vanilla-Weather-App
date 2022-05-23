f36dd0d79245227434f166397b4f323afe03e9fb
mjohnstone182 <103975325+mjohnstone182@users.noreply.github.com>
2022-05-23 16:10:51 +1000

    add new weather icons

--- src/app.js
+++ src/app.js
@@ -33,12 +33,20 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
+
+
 temperatureElement.innerHTML = Math.round (response.data.main.temp);
 cityElement.innerHTML = response.data.name;
-descriptionElement.innerHTML = response.data.weather[0].description;
+descriptionElement.innerHTML = 
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round (response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute(
+  "src",
+  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "187ee23d0b9957fa5f612328e679b48c";
